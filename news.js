787f5f16a167756d6239b61b4ef4d87d2eb54cc0
sujondev <emd26521@gmail.com>
2022-09-03 11:10:37 +0600

    add news when click

--- news.js
+++ news.js
@@ -19,16 +19,37 @@ displayCategory = categories => {
 }
 
 categoryData = (categoryId) => {
-    console.log(categoryId)
     const url = `https://openapi.programming-hero.com/api/news/category/0${categoryId}`
     fetch(url)
         .then(res => res.json())
-        .then(data => showCategoryNews(data))
+        .then(data => showCategoryNews(data.data))
         .catch(error => console.log(error))
 }
 
 showCategoryNews = (categoryNews) => {
-    console.log(categoryNews)
+    const newsContainer = document.getElementById('news-container')
+    newsContainer.textContent = ''
+    categoryNews.forEach(news => {
+        console.log(news);
+        const newsDiv = document.createElement('div');
+
+        newsDiv.innerHTML = `
+        <div class="card mt-3">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="${news.image_url}" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">${news.title}</h5>
+        <p class="card-text">${news.details.length > 200 ? news.details.slice(0, 200) + '...' : news.details}</p>
+      </div>
+    </div>
+  </div>
+</div>
+        `;
+        newsContainer.appendChild(newsDiv)
+    })
 }
 
 loadCategory()
